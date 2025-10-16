c27c7c9316d502d4111229ba00db205647036ea3
Ahmed Khaled <58346988+ahmedkhaledibrahim@users.noreply.github.com>
2025-10-16 11:31:12 +0300

    sliding window technique

---
+++ ProblemSolving/SlidingWindow/FindMaximumAverageOfSubarray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.SlidingWindow
+{
+    public static class FindMaximumAverageOfSubarray
+    {
+        public static double Execute(int[] arr, int k)
+        {
+            double maximum = 0;
+            double windowSum = 0;
+            for (int i = 0; i < k; i++)
+            {
+                maximum += arr[i];
+            }
+            windowSum = maximum;
+
+            for (int i = 0; i < arr.Length - k; i++)
+            {
+                windowSum = windowSum - arr[i] + arr[k + i];
+                maximum = Math.Max(maximum, windowSum);
+            }
+            return maximum / k;
+
+        }
+    }
+}
---
+++ ProblemSolving/SlidingWindow/FindMaximumSumOfSubarray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolving.SlidingWindow
+{
+    public static class FindMaximumSumOfSubarray
+    {
+        public static int Execute(int[] arr, int k) {
+            int maximum = 0;
+            int windowSum = 0;
+            for (int i = 0; i < k; i++)
+            {
+                maximum += arr[i];
+            }
+            windowSum = maximum;
+
+            for (int i = 0; i < arr.Length - k; i++)
+            {
+                windowSum = windowSum - arr[i] + arr[k + i];
+                maximum = Math.Max(maximum, windowSum);
+            }
+            return maximum;
+        }
+    }
+}
